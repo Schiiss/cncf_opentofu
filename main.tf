18382b26079a01ffce05c0545b861fad5e386a53
Schiiss <conner.schiissler@gmail.com>
2024-05-06 15:55:48 -0600

    removing vm

--- main.tf
+++ main.tf
@@ -52,26 +52,3 @@ resource "azurerm_subnet" "example" {
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "linuxmachine"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  disable_password_authentication = false
-  admin_password                  = var.vm_password
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
